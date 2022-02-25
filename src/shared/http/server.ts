87ae43d516b3ee853209028aa730ff5dd936d4e7
Ericson Scodeler <ericson.br@hotmail.com>
2022-02-25 18:24:48 -0300

    validacao de body e parms utilizando Celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,7 @@ import 'express-async-errors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
