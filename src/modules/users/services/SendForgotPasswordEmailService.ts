88a24b65809194c205dd827532f511284acf732a
Ericson Scodeler <ericson.br@hotmail.com>
2022-03-12 23:36:14 -0300

    Configuracao do EtherealMail que envia o token por email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository ';
 
+import EtherealMail from '@config/mail/EtherealMail';
+
 interface IRequest {
   email: string;
 }
@@ -21,6 +23,11 @@ class SendForgotPasswordEmailService {
     const token = await userTokenRepository.generate(user.id);
 
     console.log(token);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+    });
   }
 }
 
