55a0f03568914fe45279c173695a6b6b8e0e4840
Ericson Scodeler <ericson.br@hotmail.com>
2022-04-14 08:08:15 -0300

    Implementacao do rateLimiter para bloquear chamadas excessivas da api

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -9,11 +9,13 @@ import '@shared/typeorm';
 import { pagination } from 'typeorm-pagination';
 import { errors } from 'celebrate';
 import uploadConfig from '@config/upload';
+import rateLimiter from '@shared/http/middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(rateLimiter);
 app.use(pagination);
 
 app.use('/files', express.static(uploadConfig.directory));
